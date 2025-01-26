849cb26c6de44f10f6e8f9c1dfdcbafa533433b0
AlexanderOsharov <sashosharov@gmail.com>
2025-01-26 03:54:45 +0300

    Improvements

--- historical_text_classifier_xgboost/xgboost_model.py
+++ historical_text_classifier_xgboost/xgboost_model.py
@@ -3,21 +3,16 @@ import re
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, roc_curve, auc
-import matplotlib.pyplot as plt
-import seaborn as sns
-from xgboost import XGBClassifier
-from sklearn.pipeline import Pipeline
-from nltk.corpus import stopwords
-import nltk
-import chardet
-import joblib
-from os.path import join, dirname, abspath
 import requests
+import seaborn as sns
+import matplotlib.pyplot as plt
+import chardet
+from os.path import join, dirname, abspath
+import joblib
 
-# Загрузка стоп-слов один раз при инициализации класса
-nltk.download('stopwords', quiet=True)  # Отключаем сообщения о загрузке
 
 class TextPreprocessor(BaseEstimator, TransformerMixin):
     def __init__(self, stop_words):
@@ -36,12 +31,13 @@ class TextPreprocessor(BaseEstimator, TransformerMixin):
         words = [word for word in words if word not in self.stop_words]  # Удаление стоп-слов
         return ' '.join(words)
 
-class XGBoostTextClassifier:
+
+class RandomForestTextClassifier:
     def __init__(self, dataset_path=None, model_path=None):
         self.dataset_path = dataset_path or join(dirname(abspath(__file__)), 'data', 'dataset.json')
-        self.model_path = model_path or 'xgboost_model.pkl'
+        self.model_path = model_path or 'random_forest_model.pkl'
         self.pipeline = None
-        self.stop_words = set(stopwords.words('russian'))
+        self.stop_words = set(['акт', 'лист', 'приказ', 'распоряжение', '№'])
 
     def load_data(self):
         with open(self.dataset_path, 'r', encoding='utf-8') as f:
@@ -54,25 +50,19 @@ class XGBoostTextClassifier:
         texts, labels = self.load_data()
         pipeline = Pipeline(steps=[
             ('cleaner', TextPreprocessor(stop_words=self.stop_words)),
-            ('tfidf', TfidfVectorizer(max_features=5000)),  # Ограничение количества признаков
-            ('classifier', XGBClassifier(random_state=42))
+            ('tfidf', TfidfVectorizer()),
+            ('classifier', RandomForestClassifier(n_estimators=100, random_state=42))
         ])
-        param_grid = {
-            'tfidf__max_features': [5000, 10000],
-            'classifier__n_estimators': [100, 200],
-            'classifier__learning_rate': [0.01, 0.1],
-            'classifier__max_depth': [3, 6, 9]
-        }
-        grid_search = GridSearchCV(pipeline, param_grid, cv=3, scoring='accuracy', n_jobs=-1, error_score='raise')
-        grid_search.fit(texts, labels)
-        self.pipeline = grid_search.best_estimator_
+        X_train, X_test, y_train, y_test = train_test_split(texts, labels, test_size=0.2, random_state=42)
+        pipeline.fit(X_train, y_train)
+        self.pipeline = pipeline
         self.save_model()
 
     def save_model(self):
         joblib.dump(self.pipeline, self.model_path)
 
     def load_model(self):
-        if not self.pipeline:
+        if not hasattr(self, 'pipeline'):
             try:
                 self.pipeline = joblib.load(self.model_path)
             except FileNotFoundError:
@@ -88,23 +78,24 @@ class XGBoostTextClassifier:
         preprocessed_text = [text]  # Не нужно вызывать preprocess_text здесь
         return self.pipeline.predict_proba(preprocessed_text)[0]
 
-    def evaluate(self):
+    def evaluate_confusion_matrix(self):
         self.load_model()
         texts, labels = self.load_data()
         y_pred = self.pipeline.predict(texts)
-        y_prob = self.pipeline.predict_proba(texts)[:, 1]
-        print("Accuracy:", accuracy_score(labels, y_pred))
-
-        # Матрица ошибок
         cm = confusion_matrix(labels, y_pred)
         plt.figure(figsize=(8, 6))
-        sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=['Twaddle', 'Historical Background'], yticklabels=['Twaddle', 'Historical Background'])
+        sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=['Twaddle', 'Historical Background'],
+                    yticklabels=['Twaddle', 'Historical Background'])
         plt.xlabel('Предсказанные значения')
         plt.ylabel('Фактические значения')
         plt.title('Матрица ошибок')
         plt.show()
 
-        # ROC-кривая
+    def evaluate_roc_curve(self):
+        self.load_model()
+        texts, labels = self.load_data()
+        y_pred = self.pipeline.predict(texts)
+        y_prob = self.pipeline.predict_proba(texts)[:, 1]
         fpr, tpr, thresholds = roc_curve(labels, y_prob)
         roc_auc = auc(fpr, tpr)
         plt.figure(figsize=(8, 6))
@@ -118,16 +109,43 @@ class XGBoostTextClassifier:
         plt.legend(loc="lower right")
         plt.show()
 
+    def feature_importance_plot(self):
+        self.load_model()
+        feature_importances = self.pipeline.named_steps['classifier'].feature_importances_
+        feature_names = self.pipeline.named_steps['tfidf'].get_feature_names_out()
+        sorted_indices = feature_importances.argsort()[::-1]
+        sorted_importances = feature_importances[sorted_indices]
+        sorted_feature_names = feature_names[sorted_indices]
+        plt.figure(figsize=(12, 6))
+        plt.bar(range(30), sorted_importances[:30], align="center")
+        plt.xticks(range(30), sorted_feature_names[:30], rotation=90)
+        plt.xlabel("Слова")
+        plt.ylabel("Важность")
+        plt.title("Важность признаков (слова)")
+        plt.show()
+
     def extract_valuable_passages(self, input_text, threshold=0.5, min_length=10):
         paragraphs = input_text.split("\n")
         paragraphs = [p.strip() for p in paragraphs if len(p.strip().split()) >= min_length]
         paragraphs = [p for p in paragraphs if not any(kw in p.lower() for kw in self.stop_words)]
+
+        # Добавляем логи для отладки
+        print(f"Исходное количество абзацев: {len(input_text.split('\n'))}")
+        print(f"Абзацы после фильтрации по минимальной длине: {len(paragraphs)}")
+        print(f"Абзацы после фильтрации по ключевым словам: {len(paragraphs)}")
+
         if not paragraphs:
             return []  # Возвращаем пустой список, если нет подходящих абзацев
+
         preprocessed_paragraphs = [self.preprocess_text(p) for p in paragraphs]
         paragraph_vectors = self.pipeline.named_steps['tfidf'].transform(preprocessed_paragraphs)
         probabilities = self.pipeline.predict_proba(paragraph_vectors)[:, 1]
         results = [(para, prob) for para, prob in zip(paragraphs, probabilities) if prob >= threshold]
+
+        # Добавляем логи для отладки
+        print(f"Абзацы после предобработки: {len(preprocessed_paragraphs)}")
+        print(f"Абзацы с вероятностью >= {threshold}: {len(results)}")
+
         return results
 
     def fetch_data_from_wikipedia(self, query, num_results=5):
@@ -139,25 +157,6 @@ class XGBoostTextClassifier:
             return [item["snippet"] for item in results][:num_results]
         else:
             return []
-
-    def feature_importance_plot(self):
-        self.load_model()
-        booster = self.pipeline.named_steps['classifier'].get_booster()
-        importance = booster.get_score(importance_type='weight')
-        importance = dict(sorted(importance.items(), key=lambda item: item[1], reverse=True))
-        feature_names = self.pipeline.named_steps['tfidf'].get_feature_names_out()
-        feature_importances = np.array([importance.get(f, 0) for f in feature_names])
-        top_n = 30
-        sorted_indices = feature_importances.argsort()[::-1][:top_n]
-        sorted_importances = feature_importances[sorted_indices]
-        sorted_feature_names = feature_names[sorted_indices]
-        plt.figure(figsize=(12, 6))
-        plt.bar(range(top_n), sorted_importances, align="center")
-        plt.xticks(range(top_n), sorted_feature_names, rotation=90)
-        plt.xlabel("Слова")
-        plt.ylabel("Важность")
-        plt.title("Важность признаков (слова)")
-        plt.show()
 
     def retrain(self, additional_texts, additional_labels):
         self.load_model()
