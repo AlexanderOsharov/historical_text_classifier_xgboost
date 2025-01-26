3ce1ef3230526157755d5224eed966b5cebd811a
AlexanderOsharov <sashosharov@gmail.com>
2025-01-26 03:41:13 +0300

    Improvements

--- historical_text_classifier_xgboost/xgboost_model.py
+++ historical_text_classifier_xgboost/xgboost_model.py
@@ -14,6 +14,7 @@ import nltk
 import chardet
 import joblib
 from os.path import join, dirname, abspath
+import requests
 
 # Загрузка стоп-слов один раз при инициализации класса
 nltk.download('stopwords', quiet=True)  # Отключаем сообщения о загрузке
